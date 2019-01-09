df9496020d5028828db154044d21ef710eea1e90
Anmol Gupta <guptaanmol184@gmail.com>
2019-01-09 11:16:41 +0530

    test add

---
+++ 01_assignemnt/password_capture/pcap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <pcap.h>
+
+int main(int argc, char *argv[])
+{
+    char errbuf[PCAP_ERRBUF_SIZE];
+    char *dev = argv[1];
+
+    printf("Device: %s\n", dev);
+
+    pcap_t *handle;
+    handle = pcap_open_live(dev, BUFSIZ, 0, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return(2);
+    }
+
+    return(0);
+
+}
