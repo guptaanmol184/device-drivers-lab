b4e2b1781efbe2def9d3e63c999792da7d45e6b9
Anmol Gupta <guptaanmol184@gmail.com>
2019-01-23 07:07:57 +0530

    Add license, author, description

--- 01_assignemnt/hello-world-kernel-module/hello-2.c
+++ 01_assignemnt/hello-world-kernel-module/hello-2.c
@@ -19,3 +19,7 @@ static void __exit hello_2_exit(void)
 
 module_init(hello_2_init);
 module_exit(hello_2_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Anmol Gupta");
+MODULE_DESCRIPTION("Hello World!");
