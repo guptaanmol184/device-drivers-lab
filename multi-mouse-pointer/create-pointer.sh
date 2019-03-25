91b9e5f06080f25a82e5a3449ddaffd29a3e8226
Anmol Gupta <guptaanmol184@gmail.com>
2019-03-25 15:57:13 +0530

    add multi mouse pointer code

---
+++ multi-mouse-pointer/create-pointer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function getidof () {
+    # gets device string as input
+    xinput list | grep "$1" | cut -f2 | cut -d'=' -f2
+
+    return "${PIPESTATUS[1]}" # dirty hack to return the exit status of grep
+}
+
+if getidof "master2"; then
+    echo "master2 already exists. Not creating master2."
+else
+    xinput create-master "master2"
+fi
+
+
+master2id=$(getidof "master2 pointer")
+touchpadid=$(getidof "Synaptics")
+
+xinput reattach "$touchpadid" "$master2id"
---
+++ multi-mouse-pointer/delete-pointer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function getidof () {
+    # gets device string as input
+    xinput list | grep "$1" | cut -f2 | cut -d'=' -f2
+
+    return "${PIPESTATUS[1]}" # dirty hack to return the exit status of grep
+}
+
+if getidof "master2"; then
+    master2id=$(getidof "master2")
+else
+	echo "master2 does not exist. Please run create-pointer.sh fisrt."
+	exit 1
+fi
+
+master1pointerid=$(getidof "core pointer")
+master1keyboardid=$(getidof "core keyboard")
+master2id=$(getidof "master2 pointer")
+
+xinput remove-master "$master2id" AttachToMaster "$master1pointerid" "$master1keyboardid"
