e389f27b75a31c293a1fb8064e01c4f9940c34b7
Anmol Gupta <guptaanmol184@gmail.com>
2019-03-25 16:01:03 +0530

    remove keylogger binary files

--- keylogger/.keylogger.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/media/s8/dd/device-drivers-lab/keylogger/keylogger.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /media/s8/dd/device-drivers-lab/keylogger/keylogger.ko /media/s8/dd/device-drivers-lab/keylogger/keylogger.o /media/s8/dd/device-drivers-lab/keylogger/keylogger.mod.o ;  true
--- keylogger/.tmp_versions/keylogger.mod
+++
@@ -1,3 +0,0 @@
-/media/s8/dd/device-drivers-lab/keylogger/keylogger.ko
-/media/s8/dd/device-drivers-lab/keylogger/keylogger.o
-
